d89b2a257d25efd101c2b4f8242290cebc8d180e
ethan-goh <ethan.goh.ye@gmail.com>
2024-11-22 14:32:07 +0800

    Styled review display content

--- frontend/src/components/ReviewDetails.jsx
+++ frontend/src/components/ReviewDetails.jsx
@@ -2,15 +2,13 @@ import React from 'react'
 import { Rating } from 'react-simple-star-rating'
 
 const ReviewDetails = ({ review }) => {
-    
-
     return (
         <div class="bg-white shadow-lg rounded-lg p-4 w-full max-w-md mx-auto border border-slate-200">
             <h2 class="text-xl font-semibold text-slate-800">{review.title}</h2>
             <div class="mt-2">
                 <Rating
                     readonly
-                    ratingValue={review.rating}
+                    initialValue={review.rating}
                     size={20}
                     fillColor="orange"
                     emptyColor="gray"
--- frontend/src/pages/HostelReview.jsx
+++ frontend/src/pages/HostelReview.jsx
@@ -23,20 +23,48 @@ const HostelReview = () => {
         fetchReviews()
     }, [hostelLink])
 
+    const ratingReducer = (total, num) => {
+        return total + num
+    }
+
     const hostelName = hostelLink.split("-").join(" ")
+    const averageRating = ((reviews.map((r) => r.rating).reduce(ratingReducer, 0))/reviews.length).toFixed(2)
+
 
   return (
     <div>
         <div class="w-full justify-center font-poppins text-xl font-semibold place-items-center p-6">
-            <h1>Reviews for {hostelName}</h1>
+            <h1>
+                Reviews for {hostelName}{' '}
+                {reviews.length > 0 && (
+                    <span class="font-normal inline-flex items-center ml-1">
+                        {averageRating}
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor"
+                            viewBox="0 0 24 24"
+                            class="w-5 h-5 text-yellow-500 ml-1"
+                        >
+                            <path d="M12 2.1l3.09 6.26 6.91 1-5 4.87 1.18 6.88L12 17.77l-6.18 3.26L7 14.23 2 9.36l6.91-1L12 2.1z" />
+                        </svg>
+                    </span>
+                )}
+            </h1>
         </div>
-        <div>
+        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-4">
             {reviews && reviews.map((review) => 
                 <div>
                     <ReviewDetails key={review._id} review={review} />
                 </div>
             )}
         </div>
+        {reviews.length == 0 && (
+                <div class="flex items-center justify-center h-64">
+                    <h2 class="font-poppins text-xl">
+                        No reviews yet!
+                    </h2>
+                </div>
+        )}
         
     </div>
   )
